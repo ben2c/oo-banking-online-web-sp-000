01d7fb8867271721821daabc010b23a41fd01352
Benjamin Cheng <bencheng200@gmail.com>
2020-11-21 20:11:29 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -19,7 +19,7 @@ class Transfer
       return "Transaction rejected. Please check your account balance"
     else
       @receiver.deposit(@amount)
-      @sender.balance -= @amount
+      @sender.deposit(@amount*-1)
       @status = "complete"
     end
   end
