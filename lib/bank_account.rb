a59b1601711b60734093e36d3ef3c8dcf40591ba
Benjamin Cheng <bencheng200@gmail.com>
2020-11-21 19:54:40 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -2,10 +2,13 @@ class BankAccount
   attr_accessor :name, :balance, :status
 
 
-  def initialize (name = "Bob")
-    @name = name
+  def initialize (name)
+    const cant_name = name
+    @name = cant_name
     @balance = 1000
     @status = 'open'
   end
+
+
 
 end
