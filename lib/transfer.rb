dfa4884030f5e896fd03063ab3ca5f1e56266baa
Benjamin Cheng <bencheng200@gmail.com>
2020-11-21 20:23:28 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -14,7 +14,7 @@ class Transfer
   end
 
   def execute_transaction
-    if @sender.balance < @amount && @status == 'complete'
+    if !@sender.valid?
       return "Transaction rejected. Please check your account balance"
     elsif @status == 'complete'
       return "Transaction was already executed"
