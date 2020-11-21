3ad49d22b4a969049d78bad283de7548af6f60bc
Benjamin Cheng <bencheng200@gmail.com>
2020-11-21 20:29:33 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -16,7 +16,7 @@ class Transfer
 
   def execute_transaction
     binding.pry
-    if @sender.balance < @amount && @status != 'complete'
+    if @sender.balance < @amount && !valid?
       @status = 'rejected'
       return "Transaction rejected. Please check your account balance"
     elsif @status == 'complete'
