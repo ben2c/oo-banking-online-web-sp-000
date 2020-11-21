95126807ad54967f4f9cc3ccd8387d3a622dae1f
Benjamin Cheng <bencheng200@gmail.com>
2020-11-21 20:01:08 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -2,7 +2,7 @@ class Transfer
   # your code here
   attr_accessor :sender, :receiver, :status
 
-  def initialize (amount)
+  def initialize (sender, reciever, amount)
     @transfer = amount
   end
 end
